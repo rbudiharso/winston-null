c0e1d91f1c254fce3b30079fdb12debc9561c72a
Rahmat Budiharso <rbudiharso@gmail.com>
2016-02-19 15:01:32 +0700

    release v1.0.2

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 var util = require('util'),
 winston = require('winston');
 
-var NullTransport = winston.transports.NullTransport = function() {};
+var NullTransport = exports.NullTransport = function() {};
 
 util.inherits(NullTransport, winston.Transport);
+winston.transports.NullTransport = NullTransport;
 
 NullTransport.prototype.name = 'NullTransport';
 NullTransport.prototype.log = function(level, msg, meta, callback) {
   callback(null);
 };
-
-module.exports = NullTransport;
