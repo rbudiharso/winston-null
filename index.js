33823c80b34555452bc4dc80f6229f70bcd9f63e
Julian Grinblat <julian@dotcore.co.il>
2018-11-28 11:58:16 +0900

    Support winston>=2

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const { Transport, transports } = require('winston');
+const winston = require('winston');
+const compat = require('winston-compat');
+const semver = require('semver');
+
+const Transport = semver.major(winston.version) === 2 ? compat.Transport : require('winston-transport');
 
 class NullTransport extends Transport {
   constructor(opts) {
@@ -9,9 +13,9 @@ class NullTransport extends Transport {
 
   log(...args) { // in winston >= 3 and winston < 3 callback is the last argument
     const callback = args[args.length - 1];
-    callback(null);
+    callback();
   }
 }
 
-transports.NullTransport = NullTransport;
+winston.transports.NullTransport = NullTransport;
 module.exports.NullTransport = NullTransport;
--- test/transport-test.js
+++ test/transport-test.js
@@ -1,6 +1,7 @@
 const { expect } = require('chai');
 const rewire = require('rewire');
-let transport;
+const { spy } = require('sinon');
+let NullTransport;
 let winston;
 
 describe("NullTransport", function () {
@@ -8,17 +9,18 @@ describe("NullTransport", function () {
   before(function(){
     winston = require('winston');
     expect(winston.transports).to.not.have.ownProperty("NullTransport");
-    transport = require('../index').NullTransport;
+    NullTransport = require('../index').NullTransport;
     expect(winston.transports).to.have.ownProperty("NullTransport");
     removeConsole();
   });
 
   it('should exist', function () {
-    expect(transport).to.exist;
-    expect(transport).to.be.a("function");
+    expect(NullTransport).to.exist;
+    expect(NullTransport).to.be.a("function");
   });
 
   it('should be able to add it as a transport', function () {
+    const transport = new NullTransport();
     winston.add(transport);
     winston.remove(transport);
   });
@@ -27,15 +29,19 @@ describe("NullTransport", function () {
     expect(winston.transports).to.have.ownProperty("NullTransport");
   });
 
-  it('should not write anything', function (done) {
-    transport = rewire('../index').NullTransport;
+  it('should not write anything', function () {
+    const transport = new (rewire('../index').NullTransport)();
     const msg = "hi there";
 
+    spy(transport, 'log');
+
     winston.add(transport);
-    winston.log('info', msg, null, function(err) {
-      expect(err).to.be.null;
-      done();
-    });
+    winston.log('info', msg);
+
+    expect(transport.log.callCount).to.equal(1);
+    expect(transport.log.firstCall.args[0].message).to.equal(msg);
+
+    transport.log.restore();
   });
 
   function removeConsole () {
